23e2b59e19876e9d07df02cb960ca33f4ca1fd4d
Anjana Dunukara <anjanaheshanthach@gmail.com>
2022-12-27 22:07:52 +0530

    setup for deplyment

--- public/js/app.js
+++ public/js/app.js
@@ -13,9 +13,7 @@ weatherForm.addEventListener("submit", (event) => {
   messageOne.textContent = "Loading..";
   messageTwo.textContent = "";
 
-  fetch(
-    "http://localhost:3000/weather?address=" + encodeURIComponent(location)
-  ).then((response) => {
+  fetch("/weather?address=" + encodeURIComponent(location)).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
